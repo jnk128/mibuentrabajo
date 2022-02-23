b54bb820ef07945a3d88975a3f1c359e103baf1d
jnk128 <87993850+jnk128@users.noreply.github.com>
2022-02-23 14:25:18 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+/*window.onload=function() {
+	var xhr = new XMLHttpRequest();
+	xhr.onreadystatechange = function(){
+		if (xhr.status === 200){
+			document.getElementById("footers").innerHTML = xhr.responseText;
+		}
+	}
+	xhr.open("get", "footers.html", true);
+	xhr.send();
+}*/
+
+/*const foots=document.querySelector('#footers')
+fetch('footers.html')
+.then(res=>res.text())
+.then(data=>{
+	foots.innerHTML=data
+})*/
